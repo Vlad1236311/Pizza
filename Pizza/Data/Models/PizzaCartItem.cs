8d7d924a1e2c393b8d8eb25de899ac0558828265
Aleksandr <dedulyaaa52@gmail.com>
2026-06-08 14:23:00 +0300

    Седьмое видео

---
+++ Pizza/Data/Models/PizzaCartItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+
+namespace Pizza.Data.Models
+{
+    public class PizzaCartItem
+    {
+        public int id { get; set; }
+        public Food food { get; set; }
+        public int price { get; set; }
+        
+        public string PizzaCartId { get; set; }
+    }
+}
