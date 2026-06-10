a3ad6e8f59fefa2c67dddb3146d86e7690db8938
Vlad1236311 <prijmakvlad7@gmail.com>
2026-06-10 18:24:21 +0300

    Додана головна сторінка

---
+++ Pizza/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pizza.Data.interfaces;
+using Pizza.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pizza.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAllFood _foodRep;
+
+        public HomeController(IAllFood foodRep)
+        {
+            _foodRep = foodRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeFoods = new HomeViewModel {
+            favFoods = _foodRep.getFavFoods
+            };
+            return View(homeFoods);
+        }
+    }
+}
